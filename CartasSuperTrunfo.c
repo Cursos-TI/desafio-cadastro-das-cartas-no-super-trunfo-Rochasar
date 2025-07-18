ec0ee2113a24a06b7d1db75dfa3662c912e2bc3d
Rochasar <rochasar1234@gmail.com>
2025-07-18 04:35:14 +0000

    desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,73 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char cidade[100];
+    int populacao;
+    float area;
+    int codigo;
+    char estado[100];
+    float pib;
+    int turisticos;
+
+    //especificadores utilizados em suas devidas funções para a impressão do que a variável solicita
+
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estado);
+    
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite a população: \n");
+    scanf("%d", & populacao);
+
+    printf("Quantidade de pontos túristicos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Digite o pib: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o código: \n");
+    scanf("%d", &codigo);
+
+//Dados refrentes a primeira carta, onde o printf tem a função de solicitar uma informação para o usuario e o scanf repaça os dados para a maquina 
+    
+    printf("primeira carta:\n");
+    printf("estado: %s - cidade: %s \n", estado, cidade);
+    printf("população: %d - pontos túristicos: %d \n", populacao, turisticos);
+    printf("pib: %f - código: %d \n", pib, codigo);
+    
+    //informa o jogador com base as decisões do mesmo como fica a carta, sendo usado o printf para imprimir os referentes dados, as especificações e as strings para que o dado esteja correto
+    
+    char cidade[100];
+    int populacao;
+    float area;
+    int codigo;
+    char estado[100];
+    float pib;
+    int turisticos;
+
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estado);
+    
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite a população: \n");
+    scanf("%d", & populacao);
+
+    printf("Quantidade de pontos túristicos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Digite o pib: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o código: \n");
+    scanf("%d", &codigo);
+
+    printf("primeira carta:\n");
+    printf("estado: %s - cidade: %s \n", estado, cidade);
+    printf("população: %d - pontos túristicos: %d \n", populacao, turisticos);
+    printf("pib: %f - código: %d \n", pib, codigo);
+
     return 0;
 }
